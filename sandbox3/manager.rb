0c5d47ea45c51849504cfbb306f71f2a6bab5af3
Yuya Kato <yuyakato@gmail.com>
2010-01-08 19:26:49 +0900

    リファクタリング

--- sandbox3/manager.rb
+++ sandbox3/manager.rb
@@ -7,22 +7,29 @@ require "rinda/tuplespace"
 require "config"
 require "queue_logger"
 
-logger = QueueLogger.new
-logger.info("hoge")
-exit
-
 class Manager
   def initialize(port)
-    @drb_server  = DRb.start_service(nil, self)
-    @ring_space  = Rinda::TupleSpace.new
-    @ring_space.write([:name, :ZeroCloud, DRbObject.new(@drb_server.front), ""])
-    @ring_server = Rinda::RingServer.new(@ring_space, port)
+    @logger = QueueLogger.new
+    @ts     = Rinda::TupleSpace.new
 
-    @ts = Rinda::TupleSpace.new
+    @drb_server = DRb.start_service(nil, self)
+    @logger.info("dRuby server listen on #{DRb.uri}")
+
+    @ring_space = Rinda::TupleSpace.new
+    @ring_space.write([:name, :ZeroCloud, DRbObject.new(@drb_server.front), ""])
+    @logger.info("registered service")
+
+    @ring_server = Rinda::RingServer.new(@ring_space, port)
+    @logger.info("Ring server listen")
   end
 
   def ts
     return @ts
+  end
+
+  def heartbeat(name)
+    @logger.info("heartbeat from #{name}")
+    return true
   end
 end
 
--- sandbox3/node.rb
+++ sandbox3/node.rb
@@ -5,21 +5,39 @@
 require "rinda/ring"
 require "rinda/tuplespace"
 require "config"
+require "queue_logger"
 
 class Node
   def initialize(port, broadcast)
+    @logger  = QueueLogger.new
+    @manager = nil
+
     DRb.start_service
+    @logger.info("dRuby server listen on #{DRb.uri}")
+
     @finger = Rinda::RingFinger.new(broadcast, port)
   end
 
   def manager
-    ring_space = @finger.lookup_ring_any
-    sym, name, manager, desc = ring_space.read([:name, :ZeroCloud, DRbObject, nil])
-    return manager
+    unless @manager
+      ring_space = @finger.lookup_ring_any
+      sym, name, manager, desc = ring_space.read([:name, :ZeroCloud, DRbObject, nil])
+      @manager = manager
+    end
+    return @manager
   end
 end
 
 node = Node.new(PORT, BROADCAST)
-p node.manager
+
+Thread.start {
+  loop {
+    node.manager.heartbeat("node")
+    sleep 5
+  }
+}
+
+node.manager.ts.write([:job, 1])
+#p node.manager.ts.take([:job, Integer])
 
 gets
