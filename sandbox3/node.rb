c2f74c932718a14de5730092d979ee2e4392ef37
Yuya Kato <yuyakato@gmail.com>
2010-01-08 18:53:24 +0900

    リファクタリング

--- sandbox3/node.rb
+++ sandbox3/node.rb
@@ -9,11 +9,9 @@ require "config"
 DRb.start_service
 
 finger = Rinda::RingFinger.new(BROADCAST, PORT)
-finger.lookup_ring(5) { |ts|
-  p ts
-  p service = ts.read([:name, :ZeroCloud, DRbObject, nil])
-  p service[2]
-  p service[2].ts
-}
+ring_space = finger.lookup_ring_any
+sym, name, manager, desc = ring_space.read([:name, :ZeroCloud, DRbObject, nil])
+p manager
+p manager.ts
 
 gets
