9b75893c3134a0956e6d1071561e45a8068c2252
Yuya Kato <yuyakato@gmail.com>
2010-01-08 18:47:01 +0900

    リファクタリング

--- sandbox2/manager.rb
+++ sandbox2/manager.rb
@@ -8,14 +8,13 @@ require "config"
 
 DRb.start_service
 
+ring_ts     = Rinda::TupleSpace.new
+ring_server = Rinda::RingServer.new(ring_ts, PORT)
 
-p ring_ts     = Rinda::TupleSpace.new
-p ring_server = Rinda::RingServer.new(ring_ts, PORT)
-
-ts = Rinda::TupleSpace.new
+job_ts = Rinda::TupleSpace.new
 
 p service = ring_ts.read([:name, :ZeroCloud, DRbObject, nil])
 p service[2]
-p service[2].hello(ts)
+p service[2].hello(job_ts)
 
 gets
--- sandbox2/node.rb
+++ sandbox2/node.rb
@@ -13,24 +13,13 @@ class Service
   end
 end
 
-DRb.start_service
-
-
-
 service = Service.new
 DRb.start_service(nil, service)
-p DRb.uri
 
-#=begin
 finger = Rinda::RingFinger.new(BROADCAST, PORT)
-p finger
 finger.lookup_ring(5) { |ts|
   p :lookup
   ts.write([:name, :ZeroCloud, DRbObject.new(service), ""], 600)
 }
-#=end
-
-#provider = Rinda::RingProvider.new(:ZeroCloud, DRbObject.new(service), "")
-#provider.provide
 
 gets
